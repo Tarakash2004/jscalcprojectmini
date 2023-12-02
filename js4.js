644d9edce0b31bd0b4e3b5e4be141218471c0a92
TARAKASH SEN <127127843+Tarakash2004@users.noreply.github.com>
2023-12-02 09:02:51 +0530

    Add files via upload

---
+++ js4.js
@@ -0,0 +1,43 @@
+function myfunc()
+{
+    let retw = String(document.getElementById("values").value);
+    let nm = retw.length;
+    let ls = "";
+    let rs = "";
+    let n = '';
+    let text10 = 0;
+    for(let i = 0 ; i < nm ; i++ )
+    {
+        if(isNaN(retw[i]))
+        {
+            n = i;
+        }
+
+    }
+        
+        for(let j = 0 ; j < n ; j++)
+        {
+            ls += retw[j];
+        }
+        for(let k = n+1 ; k < nm ; k++)
+        {
+            rs += retw[k];
+        }
+        switch(retw[n])
+        {
+            case '+':
+                text10 = Number(ls) + Number(rs);
+                break;
+            case '-':
+                text10 = Number(ls) - Number(rs);
+                break;
+            case '/':
+                text10 = Number(ls) / Number(rs);
+                break;
+            case '%':
+                text10 = Number(ls) % Number(rs);
+        }
+       
+    
+document.getElementById("sen1").innerHTML = "your value is" + " " + "<br>" +text10;
+}
